e2270c66d6bc61d9cf5073d4e6943aec2b8c38fe
Paxton Alexander <palexander2113@gmail.com>
2019-11-16 05:10:54 -0700

    Good lookin map

--- js/map.js
+++ js/map.js
@@ -1,10 +1,11 @@
-class Map {
+class StateMap {
     /**
      * Creates a Map Object
      */
-    constructor() {
-        this.projection = d3.geoAlbersUsa().scale(1300)
-
+    constructor(data, tooltip) {
+        this.data = data;
+        this.projection = d3.geoAlbersUsa().scale(1300);
+        this.tooltip = tooltip;
     }
 
     /**
@@ -14,14 +15,41 @@ class Map {
 
     }
 
-    /**
-     * Update Map with info for a specific FIFA World Cup
-     * @param wordcupData the data for one specific world cup
-     */
-    updateMap() {
 
-        //Clear any previous selections;
-        this.clearMap();
+    updateMap(year) {
+        let total = 0;
+        let fatalities = this.data.map(s => {
+            if (s.ID !== "9999")
+                return parseInt(s[year]);
+            else
+                total = parseInt(s[year]);
+        });
+        document.getElementById('national-total').innerHTML = `Total Fatalities: ${total}`;
+        let ids = this.data.map(s => {return `s${s.ID}`});
+        let states = {};
+        ids.forEach((id, i) => {
+            states[id] = fatalities[i]
+        });
+        let colorScale = d3.scaleSequential()
+            .domain([d3.min(fatalities), d3.max(fatalities)])
+            .interpolator(d3.interpolateReds);
+
+        d3.select('.states')
+            .selectAll('path')
+            .attr("fill", function(d) { return colorScale(states[`s${parseInt(d.id)}`]); })
+            .on('click', function(d) {
+                console.log(`s${parseInt(d.id)}`);
+            })
+            .on('mouseover', (d) => {
+                this.tooltip.mouseover(states[`s${parseInt(d.id)}`])
+            })
+            .on('mouseout', (d) => {
+                this.tooltip.mouseout(states[`s${parseInt(d.id)}`])
+            })
+            .on('mousemove', (d) => {
+                this.tooltip.mousemove(states[`s${parseInt(d.id)}`])
+            })
+        ;
 
     }
 
@@ -29,28 +57,91 @@ class Map {
      * Adapted from https://bl.ocks.org/adamjanes/6cf85a4fd79e122695ebde7d41fe327f
      */
 
-    drawMap(data) {
-
-        let selected = document.getElementById('year').value;
-        let fatalities = data.map(s => {return parseInt(s[selected])});
-        let ids = data.map(s => {return `s${s.ID}`});
+    drawMap() {
+        let year = document.getElementById('year').value;
+        let total = 0;
+        let fatalities = this.data.map(s => {
+            if (s.ID !== "9999")
+                return parseInt(s[year]);
+            else
+                total = parseInt(s[year]);
+        });
+        document.getElementById('national-total').innerHTML = `Total Fatalities: ${total}`;
+        let ids = this.data.map(s => {return `s${s.ID}`});
         let states = {};
-        ids.forEach((id, i) => states[id] = fatalities[i]);
-
-        console.log(states);
-
-        let colorScale = d3.scaleOrdinal()
+        ids.forEach((id, i) => {
+            states[id] = fatalities[i]
+        });
+        let colorScale = d3.scaleSequential()
             .domain([d3.min(fatalities), d3.max(fatalities)])
-            .range(d3.schemeReds[9]);
-
-        // let colorScale = d3.scaleQuantize()
-        //     .domain([d3.min(fatalities),d3.max(fatalities)])
-        //     .range(["#1e99be", "#2b8cbe","#3138bd", "#0a0fc2", "#020053"])
+            .interpolator(d3.interpolateReds);
 
         let map = d3.select("#map")
             .attr('width', 960)
             .attr('height', 600)
         ;
+
+        let key = map
+            .append("g")
+            .attr("width", 300)
+            .attr("height", 30);
+
+        let legend = key.append("defs")
+            .append("svg:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "0%")
+            .attr("y1", "100%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+        legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", "#fff3ed")
+            .attr("stop-opacity", 1);
+
+        legend.append("stop")
+            .attr("offset", "33%")
+            .attr("stop-color", "#fcb69e")
+            .attr("stop-opacity", 1);
+
+        legend.append("stop")
+            .attr("offset", "66%")
+            .attr("stop-color", "#d86c69")
+            .attr("stop-opacity", 1);
+
+        legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", "#4f000d")
+            .attr("stop-opacity", 1);
+
+        key.append("rect")
+            .attr("width", 300)
+            .attr("height", 50 - 30)
+            .style("fill", "url(#gradient)")
+            .attr("transform", "translate(600,0)");
+
+        let y = d3.scaleLinear()
+            .domain([4000, 0])
+            .range([300, 0])
+        ;
+
+        let yAxis = d3.axisBottom()
+            .scale(y)
+            .ticks(5);
+
+        key.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(600,20)")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("axis title");
+
+
 
         let path = d3.geoPath();
         d3.json("https://d3js.org/us-10m.v1.json")
@@ -60,12 +151,23 @@ class Map {
                     .selectAll("path")
                     .data(topojson.feature(us, us.objects.states).features)
                     .enter().append("path")
-                    .attr("fill", function(d) { return colorScale(states[`s${d.id}`]); })
-                    .attr('id', function(d) {return `s${d.id}`})
+                    .attr("fill", function(d) { return colorScale(states[`s${parseInt(d.id)}`]); })
+                    .attr('id', function(d) {
+                        return `s${parseInt(d.id)}`
+                    })
                     .attr("d", path)
 
                     .on('click', function(d) {
-                        console.log(`s${d.id}`);
+                        console.log(`s${parseInt(d.id)}`);
+                    })
+                    .on('mouseover', (d) => {
+                        this.tooltip.mouseover(states[`s${parseInt(d.id)}`])
+                    })
+                    .on('mouseout', (d) => {
+                        this.tooltip.mouseout(states[`s${parseInt(d.id)}`])
+                    })
+                    .on('mousemove', (d) => {
+                        this.tooltip.mousemove(states[`s${parseInt(d.id)}`])
                     })
                 ;
 
@@ -75,8 +177,6 @@ class Map {
                     .attr("d", path)
                 ;
             })
-
-
 
     }
 
--- js/script.js
+++ js/script.js
@@ -1,15 +1,44 @@
-let map = new Map();
+let map;
+let opts = {
+    lines: 9, // The number of lines to draw
+    length: 9, // The length of each line
+    width: 5, // The line thickness
+    radius: 14, // The radius of the inner circle
+    color: '#2d65bd', // #rgb or #rrggbb or array of colors
+    speed: 1.9, // Rounds per second
+    trail: 40, // Afterglow percentage
+    className: 'spinner', // The CSS class to assign to the spinner
+};
+let spinnerConfig = {
+    target : 'spinner',
+    width: 900,
+    height: 450,
+    val: 90
+};
+let target = document.getElementById(spinnerConfig.target);
 
+function init() {
+    let spinner = new Spinner(opts).spin(target);
+    let tooltip = new Tooltip();
 
+    Promise.all([
+        d3.csv("data/fatality-rate.csv"),
+        d3.csv("data/fatalities-per-100-mvt.csv"),
+    ]).then(function (files) {
+        map = new StateMap(files[0], tooltip);
+        map.drawMap();
+        console.log(files[0]);
+        console.log(files[1]);
+        spinner.stop();
+    }).catch(function (err) {
+        console.log(err);
+        spinner.stop();
+    });
+}
 
-Promise.all([
-    d3.csv("data/fatality-rate.csv"),
-    d3.csv("data/fatalities-per-100-mvt.csv"),
-]).then(function(files) {
-    map.drawMap(files[0]);
-    console.log(files[0]);
-    console.log(files[1])
-    // files[1] will contain file2.csv
-}).catch(function(err) {
-    console.log(err)
-});
+function updateData() {
+    let selected = document.getElementById('year').value;
+    map.updateMap(selected)
+}
+
+init();
